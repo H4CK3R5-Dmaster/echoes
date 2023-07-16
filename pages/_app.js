db40e9ef24b937079ae565adcf78b9f43288c567
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2023-07-16 22:07:28 +0200

    feat: analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,14 @@ import { ChakraProvider } from '@chakra-ui/react'
 import theme from '@/utils/chakra-theme'
 import "../public/fonts.css"
 import PublicLayout from '@/components/layouts/PublicLayout'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <PublicLayout>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
+        <Analytics/>
       </ChakraProvider>
     </PublicLayout>
   )
