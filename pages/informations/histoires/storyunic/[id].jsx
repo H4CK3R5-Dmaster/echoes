0a01c0dd1aa2120680180e28bfb3663b59cd934b
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2023-07-26 14:31:30 +0200

    feat: informations

---
+++ pages/informations/histoires/storyunic/[id].jsx
@@ -0,0 +1,9 @@
+import ContainerBlock from "@/components/ContainerBlock";
+
+export default function Story() {
+    return(
+        <ContainerBlock>
+            
+        </ContainerBlock>
+    )
+}
