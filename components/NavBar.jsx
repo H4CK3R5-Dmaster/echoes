69e8f4f79c0d2ef6159e7d3f8fa22f4bb74039e6
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2023-07-28 12:29:53 +0200

    fix: navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,147 +1,167 @@
-import { Box, Flex, Spacer, IconButton, Collapse, useDisclosure, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Spacer,
+  IconButton,
+  Collapse,
+  useDisclosure,
+  Image,
+  Button,
+} from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import NextLink from "next/link";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
-    const { isOpen, onToggle } = useDisclosure();
-    const [isScrolled, setIsScrolled] = useState(false);
-    const [showDropdown, setShowDropdown] = useState(false);
+  const { isOpen, onToggle } = useDisclosure();
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const router = useRouter();
 
-    const handleDropdownToggle = () => {
-        setShowDropdown(!showDropdown);
+  const handleDropdownToggle = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const handleDropdownClose = () => {
+    setShowDropdown(false);
+  };
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+    const isScrolled = currentScrollY > 50;
+    setIsScrolled(isScrolled);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
     };
+  }, []);
 
-    const handleDropdownClose = () => {
-        setShowDropdown(false);
-    };
-
-    const handleScroll = () => {
-        const currentScrollY = window.scrollY;
-        const isScrolled = currentScrollY > 50;
-        setIsScrolled(isScrolled);
-    };
-
-    useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-
-    return (
-        <Box
-            bg={isScrolled ? "#5165F6" : "transparent"}
-            boxShadow={isScrolled ? "md" : "none"}
-            py={4}
-            px={8}
-            position="fixed"
-            top={isScrolled ? 0 : 0}
-            left={0}
-            right={0}
-            zIndex={isScrolled ? "sticky" : "initial"}
-            transition="top 0.3s ease-in-out"
-        >
-            <Flex align="center">
-                <Box fontWeight="bold" fontSize="lg">
-                    <NextLink href={"/"}>
-                        <Image
-                            borderRadius="full"
-                            boxSize="50px"
-                            src="/logo.png"
-                            alt="logo"
-                        />
-                    </NextLink>
-                </Box>
-                <Spacer />
-                <Box display={{ base: "none", md: "block" }}>
-                    <Flex color="white">
-                        <Box mx={4}>
-                            <NextLink href={"/"}>Accueil</NextLink>
-                        </Box>
-                        <Box mx={4}>
-                            <Box
-                                onMouseEnter={handleDropdownToggle}
-                                onMouseLeave={handleDropdownClose}
-                                position="relative"
-                            >
-                                <NextLink href={"/informations"}>Informations</NextLink>
-                                {showDropdown && (
-                                    <Box
-                                        position="absolute"
-                                        top="100%"
-                                        left={0}
-                                        width="100%"
-                                        py={2}
-                                        bg="#5165F6"
-                                        boxShadow="md"
-                                        zIndex="dropdown"
-                                    >
-                                        <Flex direction="column" color="white" fontSize={"14"}>
-                                            <Box mx={4} my={2}>
-                                                <NextLink href={"/informations/histoires"}>Histoire</NextLink>
-                                            </Box>
-                                            <Box mx={4} my={2}>
-                                                <NextLink href={"/concepte"}>Concepte</NextLink>
-                                            </Box>
-                                            <Box mx={4} my={2}>
-                                                <NextLink href={"/personnages"}>Héros</NextLink>
-                                            </Box>
-                                        </Flex>
-                                    </Box>
-                                )}
-                            </Box>
-                        </Box>
-                        <Box mx={4}>
-                            <NextLink href={"/"}>Règle</NextLink>
-                        </Box>
-                        <Box mx={4}>
-                            <NextLink href={"/"}>Commandes</NextLink>
-                        </Box>
-                        <Box mx={4}>
-                            <NextLink href={"/"}>Fun</NextLink>
-                        </Box>
-                    </Flex>
-                </Box>
-                <IconButton
-                    icon={<HamburgerIcon />}
-                    variant="ghost"
-                    colorScheme="#5165F6"
-                    size="md"
-                    aria-label={isOpen ? "Close menu" : "Open menu"}
-                    display={{ base: "block", md: "none" }}
-                    onClick={onToggle}
-                />
-            </Flex>
-            <Collapse in={isOpen} animateOpacity>
-                <Box mt={4} bg={"#5165F6"}>
-                    <Flex direction="column" color="white">
-                        <Box mx={4} my={2}>
-                            <NextLink href={"/"}>Accueil</NextLink>
-                        </Box>
-                        <Box mx={4} my={2}>
-                            <NextLink href={"/histoire"}>Histoire</NextLink>
-                        </Box>
-                        <Box mx={4} my={2}>
-                            <NextLink href={"/concepte"}>Concepte</NextLink>
-                        </Box>
-                        <Box mx={4} my={2}>
-                            <NextLink href={"/personnages"}>Héros</NextLink>
-                        </Box>
-                        <Box mx={4} my={2}>
-                            <NextLink href={"/"}>Règle</NextLink>
-                        </Box>
-                        <Box mx={4} my={2}>
-                            <NextLink href={"/"}>Commandes</NextLink>
-                        </Box>
-                        <Box mx={4} my={2}>
-                            <NextLink href={"/"}>Fun</NextLink>
-                        </Box>
-                    </Flex>
-                </Box>
-            </Collapse>
+  return (
+    <Box
+      bg={isScrolled ? "#5165F6" : "transparent"}
+      boxShadow={isScrolled ? "md" : "none"}
+      py={4}
+      px={8}
+      position="fixed"
+      top={isScrolled ? 0 : 0}
+      left={0}
+      right={0}
+      zIndex={isScrolled ? "sticky" : "initial"}
+      transition="top 0.3s ease-in-out"
+    >
+      <Flex align="center">
+        {router.asPath.startsWith("/informations/histoires/storyunic/") && ( // Vérifier le chemin de l'URL
+          <Box mx={4} color="white">
+            <NextLink href={"/informations/histoires"}>
+              <Button>Retour</Button>
+            </NextLink>
+          </Box>
+        )}
+        <Box fontWeight="bold" fontSize="lg">
+          <NextLink href={"/"}>
+            <Image
+              borderRadius="full"
+              boxSize="50px"
+              src="/logo.png"
+              alt="logo"
+            />
+          </NextLink>
         </Box>
-    );
+        <Spacer />
+        <Box display={{ base: "none", md: "block" }}>
+          <Flex color="white">
+            <Box mx={4}>
+              <NextLink href={"/"}>Accueil</NextLink>
+            </Box>
+            <Box mx={4}>
+              <Box
+                onMouseEnter={handleDropdownToggle}
+                onMouseLeave={handleDropdownClose}
+                position="relative"
+              >
+                <NextLink href={"/informations"}>Informations</NextLink>
+                {showDropdown && (
+                  <Box
+                    position="absolute"
+                    top="100%"
+                    left={0}
+                    width="100%"
+                    py={2}
+                    bg="#5165F6"
+                    boxShadow="md"
+                    zIndex="dropdown"
+                  >
+                    <Flex direction="column" color="white" fontSize={"14"}>
+                      <Box mx={4} my={2}>
+                        <NextLink href={"/informations/histoires"}>
+                          Histoire
+                        </NextLink>
+                      </Box>
+                      <Box mx={4} my={2}>
+                        <NextLink href={"/concepte"}>Concepte</NextLink>
+                      </Box>
+                      <Box mx={4} my={2}>
+                        <NextLink href={"/personnages"}>Héros</NextLink>
+                      </Box>
+                    </Flex>
+                  </Box>
+                )}
+              </Box>
+            </Box>
+            <Box mx={4}>
+              <NextLink href={"/"}>Règle</NextLink>
+            </Box>
+            <Box mx={4}>
+              <NextLink href={"/"}>Commandes</NextLink>
+            </Box>
+            <Box mx={4}>
+              <NextLink href={"/"}>Fun</NextLink>
+            </Box>
+          </Flex>
+        </Box>
+        <IconButton
+          icon={<HamburgerIcon />}
+          variant="ghost"
+          colorScheme="#5165F6"
+          size="md"
+          aria-label={isOpen ? "Close menu" : "Open menu"}
+          display={{ base: "block", md: "none" }}
+          onClick={onToggle}
+        />
+      </Flex>
+      <Collapse in={isOpen} animateOpacity>
+        <Box mt={4} bg={"#5165F6"}>
+          <Flex direction="column" color="white">
+            <Box mx={4} my={2}>
+              <NextLink href={"/"}>Accueil</NextLink>
+            </Box>
+            <Box mx={4} my={2}>
+              <NextLink href={"/histoire"}>Histoire</NextLink>
+            </Box>
+            <Box mx={4} my={2}>
+              <NextLink href={"/concepte"}>Concepte</NextLink>
+            </Box>
+            <Box mx={4} my={2}>
+              <NextLink href={"/personnages"}>Héros</NextLink>
+            </Box>
+            <Box mx={4} my={2}>
+              <NextLink href={"/"}>Règle</NextLink>
+            </Box>
+            <Box mx={4} my={2}>
+              <NextLink href={"/"}>Commandes</NextLink>
+            </Box>
+            <Box mx={4} my={2}>
+              <NextLink href={"/"}>Fun</NextLink>
+            </Box>
+          </Flex>
+        </Box>
+      </Collapse>
+    </Box>
+  );
 };
 
 export default Navbar;
